532f1fb9ddb61c2ff3c3d18900624355eff1ea60
Anthony Lovisi <anthonylovisi@gmail.com>
2024-06-24 18:24:29 +0200

    mise en place Header et Footer

---
+++ kasa/src/pages/a_propos/index.jsx
@@ -0,0 +1,13 @@
+
+
+
+
+function A_propos() {
+    return (
+        <div>
+            <h1>A propos</h1>
+        </div>
+    );
+}
+
+export default A_propos
---
+++ kasa/src/pages/fiche_appartement/index.jsx
@@ -0,0 +1,12 @@
+
+
+
+function Fiche_appartement() {
+    return (
+        <div>
+            <h1>Fiche appartement</h1>
+        </div>
+    );
+}
+
+export default Fiche_appartement
