5f15663426ac0a351835df623e5786a1fd06175e
Anthony Lovisi <anthonylovisi@gmail.com>
2024-06-27 20:15:46 +0200

    Mise en place de la page des appartements en static et création des component associés

---
+++ kasa/src/components/ApartmentBanner/index.jsx
@@ -0,0 +1,13 @@
+import './ApartmentBanner.scss'
+
+function ApartmentBanner() {
+    return (
+        
+            <div className="apartment-banner">
+            <img src="https://picsum.photos/800/400" alt="pic test" />
+            </div>
+
+    )
+}
+
+export default ApartmentBanner
